6471df0cd89bdb3faefea20e648806cb16b4ccf4
Andrii <68606253+Envil2001@users.noreply.github.com>
2022-11-17 18:28:45 -1200

    update image operator 5

--- src/components/UserInfo/index.jsx
+++ src/components/UserInfo/index.jsx
@@ -3,11 +3,10 @@ import { Link } from 'react-router-dom';
 import styles from './UserInfo.module.scss';
 
 export const UserInfo = ({ avatarUrl, fullName, additionalText, _id }) => {
-  console.log(avatarUrl);
-  console.log(avatarUrl ? 'Есть' : 'пусто');
+
   return (
     <div className={styles.root}>
-      <img className={styles.avatar} src={avatarUrl ? 'https://envil-blog-mern.herokuapp.com/uploads/' + avatarUrl : '/noavatar.png'} alt={fullName} />
+      <img className={styles.avatar} src={`https://envil-blog-mern.herokuapp.com/uploads/' ${avatarUrl}` || '/noavatar.png'} alt={fullName} />
       <div className={styles.userDetails}>
         <Link to={`/user/${_id}`} className={styles.userName}>{fullName}</Link>
         <span className={styles.additional}>{additionalText}</span>
